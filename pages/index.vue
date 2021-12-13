23a07175fc9a1741d948d7397c1c34f4393844fc
Kelvin Ngunyi <ngunyimacharia@gmail.com>
2021-12-13 09:48:32 +0200

    Floating video player configured

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="w-4/5 mx-auto relative py-16 bg-white overflow-hidden">
-    <dummy-content />
+  <div>
+      <video
+        id="floating-player"
+        autoplay
+        class="cld-video-player cld-video-player-skin-dark w-1/2 mx-auto h-96"
+      >
+      </video>
+      <div class="w-4/5 mx-auto relative py-16 bg-white overflow-hidden">
+        <dummy-content />
+      </div>
   </div>
 </template>
 
@@ -11,6 +19,17 @@ import DummyContent from "../components/DummyContent.vue"
 export default {
   components:{
     DummyContent
+  },
+  mounted(){
+    const cld = cloudinary.Cloudinary.new({ cloud_name: process.env.NUXT_ENV_CLOUDINARY_CLOUD_NAME, secure:true });
+    var player = cld.videoPlayer(
+      'floating-player',
+      {
+        controls:true,
+        floatingWhenNotVisible: 'left',
+      }
+      );
+    player.source('nuxtjs-floating-video-player/pexels-lisa-9963201.mp4');
   }
 }
 </script>
